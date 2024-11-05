ad0fa8d84497ae7041816c20c365330e88c4ad84
Issacck <122612159+ISCACK@users.noreply.github.com>
2024-11-05 11:22:50 +0100

    q132r4

---
+++ Assets/DeathCounterUI.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DeathCounterUI : MonoBehaviour
+{
+    private Text deathCounterText;
+    private int deathCount;
+
+    private void Start()
+    {
+        deathCounterText = GetComponent<Text>();
+
+        // Check if this is a new game launch (not a scene reload)
+        if (!PlayerPrefs.HasKey("GameStarted"))
+        {
+            // This is a fresh game start, reset the death count
+            PlayerPrefs.SetInt("DeathCount", 0);
+            PlayerPrefs.SetInt("GameStarted", 1); // Mark the game as started
+        }
+
+        // Load the current death count
+        deathCount = PlayerPrefs.GetInt("DeathCount", 0);
+        UpdateDeathCounterText();
+    }
+
+    public void IncrementDeathCount()
+    {
+        // Increment the death count and save it
+        deathCount++;
+        PlayerPrefs.SetInt("DeathCount", deathCount);
+        UpdateDeathCounterText();
+    }
+
+    private void UpdateDeathCounterText()
+    {
+        deathCounterText.text = "Deaths: " + deathCount;
+    }
+}
--- Assets/Scripts/Death.cs
+++ Assets/Scripts/Death.cs
@@ -1,31 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Death : MonoBehaviour
 {
-    public int Respawn;
+    private DeathCounterUI deathCounterUI;
 
-
-    // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        // Find the DeathCounterUI in the scene
+        deathCounterUI = FindObjectOfType<DeathCounterUI>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
-    }
-
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player"))
+        if (collision.CompareTag("Player"))
         {
-            SceneManager.LoadScene(Respawn);
+            // Increment the death count
+            deathCounterUI.IncrementDeathCount();
+
+            // Reload the scene
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
         }
     }
-
 }
