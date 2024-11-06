57b0ae6124daa3e768cb83a4a2381b443c4b4faa
Soheyl656 <Soheylizadkhah@elev.ga.lbs.se>
2024-11-06 13:48:39 +0100

    MeLolz

--- Assets/Scripts/door.cs
+++ Assets/Scripts/door.cs
@@ -1,27 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class door : MonoBehaviour
 {
     public bool locked;
 
+    private Animator anim;
+
+    [SerializeField] GameObject Player;
+
     // Start is called before the first frame update
     void Start()
     {
+        anim = GetComponent<Animator>();
         locked = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float distance = Vector2.Distance(Player.transform.position, transform.position);
+
+            if (!locked && distance < 0.5f)
+            {
+             SceneManager.LoadScene(2);
+            }
     }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.CompareTag("Key"))
         {
+            anim.SetTrigger("Open"); 
             locked = false; 
         }
     }
@@ -30,6 +42,7 @@ public class door : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Key"))
         {
+            anim.SetTrigger("Closed");
             locked = true; 
         }
     }
