bc51bcba2cba3007bcdba09ec085e77b9190974f
Soheyl656 <Soheylizadkhah@elev.ga.lbs.se>
2024-11-08 10:55:46 +0100

    Wowz

---
+++ Assets/SettingsMenu.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+
+    [SerializeField] private AudioMixer myMixer;
+    [SerializeField] private Slider musicSlider;
+
+}
